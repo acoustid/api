ce18ee95152e3b05d1fec661878246d5ea7cd43f
Lukáš Lalinský <lukas@oxygene.sk>
2016-11-15 00:39:25 +0100

    Benchmark PopCount

--- util/popcount_test.go
+++ util/popcount_test.go
@@ -39,3 +39,43 @@ func TestPopCount64(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkPopCount32(b *testing.B) {
+	var bits [32]uint
+	for i := 0; i < 32; i++ {
+		bits[i] = uint(i)
+	}
+	var x [1024]uint32
+	for i := range x {
+		p := rand.Perm(32)
+		b := rand.Int() % 32
+		for k := range p[:b] {
+			x[i] |= uint32(1) << bits[k]
+		}
+	}
+	b.ResetTimer()
+	b.SetBytes(4)
+	for i := 0; i < b.N; i++ {
+		PopCount32(x[i%len(x)])
+	}
+}
+
+func BenchmarkPopCount64(b *testing.B) {
+	var bits [64]uint
+	for i := 0; i < 64; i++ {
+		bits[i] = uint(i)
+	}
+	var x [1024]uint64
+	for i := range x {
+		p := rand.Perm(64)
+		b := rand.Int() % 64
+		for k := range p[:b] {
+			x[i] |= uint64(1) << bits[k]
+		}
+	}
+	b.ResetTimer()
+	b.SetBytes(8)
+	for i := 0; i < b.N; i++ {
+		PopCount64(x[i%len(x)])
+	}
+}
