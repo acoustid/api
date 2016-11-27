09803a4e3f557895a230aaa711dd8600589b366b
Lukáš Lalinský <lukas@oxygene.sk>
2016-11-27 10:47:21 +0100

    SQLite-like variable integer encoding

--- util/varint.go
+++ util/varint.go
@@ -40,3 +40,52 @@ func Uvarint32(buf []byte) (uint32, int) {
 	}
 	return 0, 0
 }
+
+// http://www.sqlite.org/src4/doc/trunk/www/varint.wiki
+
+func PutSQLiteUvarint32(buf []byte, x uint32) int {
+	if x < 245 {
+		buf[0] = byte(x)
+		return 1
+	}
+	if x < 2292 {
+		buf[0] = byte((x - 244)/256 + 245)
+		buf[1] = byte((x - 244) % 256)
+		return 2
+	}
+	if x < 67828 {
+		buf[0] = 253
+		buf[1] = byte((x - 2292) / 256)
+		buf[2] = byte((x - 2292) % 256)
+		return 3
+	}
+	if x < 1<<24 {
+		buf[0] = 254
+		buf[1] = byte(x >> 16)
+		buf[2] = byte(x >> 8)
+		buf[3] = byte(x)
+		return 4
+	}
+	buf[0] = 255
+	buf[1] = byte(x >> 24)
+	buf[2] = byte(x >> 16)
+	buf[3] = byte(x >> 8)
+	buf[4] = byte(x)
+	return 5
+}
+
+func SQLiteUvarint32(buf []byte) (uint32, int) {
+	if buf[0] <= 244 {
+		return uint32(buf[0]), 1
+	}
+	if buf[0] <= 252 {
+		return 244 + 256*(uint32(buf[0])-245) + uint32(buf[1]), 2
+	}
+	if buf[0] == 253 {
+		return 2292 + 256*uint32(buf[1]) + uint32(buf[2]), 3
+	}
+	if buf[0] == 254 {
+		return uint32(buf[1])<<16 | uint32(buf[2])<<8 | uint32(buf[3]), 4
+	}
+	return uint32(buf[1])<<24 | uint32(buf[2])<<16 | uint32(buf[3])<<8 | uint32(buf[4]), 5
+}
